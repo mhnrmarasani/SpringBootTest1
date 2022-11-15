d90840a4697168b399747765197f8eb9d09d430d
mhnrmarasani <118293182+mhnrmarasani@users.noreply.github.com>
2022-11-15 21:42:18 +0530

    Add files via upload

---
+++ TestArtifactID/src/main/java/HelloWorld1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author hp
+ *
+ */
+public class HelloWorld1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World again");
+		System.out.println("Second hello");
+		
+	}
+
+}
